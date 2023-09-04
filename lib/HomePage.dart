b19cde11ef60e6740b7df1354441d0d50ad1b910
sanduni <sandunissts@gmail.com>
2023-09-04 13:58:59 +0530

    create the qr scanner

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,6 +11,23 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  QRViewController? controller;
+  TextEditingController qrTextController = TextEditingController();
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        qrTextController.text = scanData.code!;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,13 +36,59 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
         backgroundColor: Colors.blue[400],
         title: const Text(
-          'Barcode Reader',
+          'QR code Scanner',
           style: TextStyle(
             color: Colors.black,
             fontStyle: FontStyle.italic,
             fontSize: 30,
           ),
         ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+              flex: 5,
+              child: QRView(
+                key: qrKey,
+                onQRViewCreated: _onQRViewCreated,
+              )),
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                controller: qrTextController,
+                readOnly: true,
+                decoration: InputDecoration(
+                  labelText: 'QR Code',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    if (qrTextController.text.isNotEmpty) {
+                      Clipboard.setData(
+                          ClipboardData(text: qrTextController.text));
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('Copied to Clipboard'),
+                        ),
+                      );
+                    }
+                  },
+                  child: Text('Copy'),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
